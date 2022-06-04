39389abd2997d3a421b4929ecda91ada09941796
Nhân Phan <me@nhanphan.net>
2022-06-04 16:33:12 +0000

    Update error middle handle wrong object id and validation error

--- src/app/middlewares/error.middleware.ts
+++ src/app/middlewares/error.middleware.ts
@@ -1,12 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import httpStatus from 'http-status';
+import { ApiErrorHandler } from 'app/helpers';
 
 const errorMiddleware = (err: any, _: NextApiRequest, res: NextApiResponse) => {
-  const error = {
+  let error = {
     ...err,
     statusCode: err.statusCode || httpStatus.INTERNAL_SERVER_ERROR,
     message: err.message,
   };
+
+  // Handle wrong mongoose object id error
+  if (err.name === 'CastError') {
+    error = {
+      message: `Resource not found. Invalid: ${err.path}`,
+      statusCode: 400,
+    };
+  }
+
+  // Handling mongoose validation error
+  if (err.name === 'ValidationError') {
+    error = {
+      message: Object.values(err.errors).map(
+        (errorItem) => (errorItem as any).message,
+      ),
+      statusCode: 400,
+    };
+  }
 
   res.status(error.statusCode);
   res.json({
